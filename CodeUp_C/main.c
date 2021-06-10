27bbffeab1e77f6fa7a858013262f392bd5db794
전호영 <78394999+HoyeongJeon@users.noreply.github.com>
2021-06-10 23:50:34 +0900

    Initial Commit

---
+++ CodeUp_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CodeUp_C
+//
+//  Created by Hoyoung Jeon on 2021/06/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
